3a62991d2ee2a7c318c8af420be916e003d5b499
Jesse <jessewilshier98@gmail.com>
2020-08-19 14:01:51 +1200

    Set up file browser to read fake data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  @ViewChild('fileBrowser', {static: false}) fileBrowser;
 
   title = 'Educational-Reward-System';
 
@@ -15,5 +17,25 @@ export class AppComponent {
     {fillStyle : '#7de6ef', text : 'Prize 3'},
     {fillStyle : '#e7706f', text : 'Prize 4'}
   ];
+
+  constructor() {}
+
+  /* Runs when user selects file for student data */
+  public fileSelected(studentFile) {
+    // Initialize a new file reader
+    const reader: FileReader = new FileReader();
+    // Get the content of the file
+    reader.readAsText(studentFile);
+    // When reader is finished loading
+    reader.onload = ((e: any) => {
+      // Split the data up into rows
+      const studentData = e.target.result.split('\n');
+      // Delete the first row (It's just the column names)
+      studentData.splice(0, 1);
+    });
+  }
+
+  ngOnInit() {
+  }
 
 }
